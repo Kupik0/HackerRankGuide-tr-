cf52657d5d4cf275203ff84cb30f5b73c0389fa9
Mustafa Kupik <111908528+Kupik0@users.noreply.github.com>
2023-04-05 21:18:28 +0300

    Grading Studens

---
+++ GradingStudents/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    { int yuvarlanmisDeger = 0;
+     List<int> finalGrades = new List<int>();
+        for(int i = 0; i < grades.Count; i++)
+        {
+            if (grades[i]> 37 && 5 - grades[i] % 5 < 3)
+            {
+                finalGrades.Add(grades[i] + 5 - grades[i] % 5);
+            }
+            else
+            {
+                finalGrades.Add(grades[i]);
+            }
+        }
+        return finalGrades;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       
+
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        Console.WriteLine(String.Join("\n", result));
+
+    }
+}
